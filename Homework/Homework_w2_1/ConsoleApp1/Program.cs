ec1902752eecd358daae758acbc964e005c3ba7c
opilane <opilane@HP-CZC1339D2N>
2024-04-19 14:52:52 +0300

    Hi

---
+++ Homework/Homework_w2_1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result1 = FindPercentage(40, 50 );
+            Console.WriteLine(result1);
+
+            double result2 = FindPercentage(30, 1);
+            Console.WriteLine(result2);
+
+        }
+        public static double FindPercentage(double first,double second)
+		{
+            double result  = 0;
+            result = first * second / 100;
+
+            return result;
+		}
+    }
+}
---
+++ Homework/Homework_w2_1/Homework_w2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework_w2_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			AskUser();
+		}
+		public static void AskUser() 
+		{
+			List<string> words = new List<string>();
+			Console.WriteLine("input 3 words");
+			words.Add(Console.ReadLine());
+			words.Add(Console.ReadLine());
+			words.Add(Console.ReadLine());
+			words.Sort();
+			foreach (string i in words)
+			{
+				Console.WriteLine(i);
+			}
+
+
+
+
+
+
+
+
+		}
+	}
+}
