bd82a6e62c899dd92801a164095db510512c2379
opilane <opilane@HP-CZC1339D2N>
2024-04-19 15:13:09 +0300

    Hi

---
+++ Homework/Homework_w2/Homework_w2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_w2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result1 = FindPercentage(40, 50 );
+            Console.WriteLine(result1);
+
+            double result2 = FindPercentage(30, 1);
+            Console.WriteLine(result2);
+
+        }
+        public static double FindPercentage(double first,double second)
+		{
+            double result  = 0;
+            result = first * second / 100;
+
+            return result;
+		}
+    }
+}
