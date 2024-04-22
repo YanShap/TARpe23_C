df3ad954238030bf41329436c3b4318f06c30f51
YanShap <158562027+YanShap@users.noreply.github.com>
2024-04-22 13:56:22 +0300

    Add files via upload

---
+++ Homework/Homework_w2/Homework_w2_4/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int secretNumber = 7;
+
+        Console.WriteLine("Arva ära number millest ma mõtlen:");
+
+        while (!GuessNumber(secretNumber))
+        {
+            Console.WriteLine("Vale number proovi uuesti:");
+        }
+
+        Console.WriteLine("Õige sa arvasid mu numbri ära!");
+    }
+
+    static bool GuessNumber(int secretNumber)
+    {
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int guess))
+        {
+            if (guess == secretNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Tundmatu number kirjuta tavaline number :");
+            return false;
+        }
+    }
+}
---
+++ Homework/Homework_w2/Homework_w2_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PrintCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintCharacters();
+        }
+        public static void PrintCharacters()
+        {
+            for (int i = 7; i > 0; i--)
+            {
+                for (int a = 0; a < i; a++)
+
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
