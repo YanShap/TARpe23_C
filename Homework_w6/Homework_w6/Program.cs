217193c9238c9b3cc1e018a15a1bcf430131b571
YanShap <158562027+YanShap@users.noreply.github.com>
2024-05-17 20:51:02 +0300

    Add files via upload

---
+++ Homework_w6/Homework_w6/Program.cs
@@ -0,0 +1,71 @@
+﻿
+internal class Circle
+{
+    private double _radius;
+    private double _diameter;
+
+    static void Main(string[] args)
+    {
+        Circle circle1 = new Circle();
+        circle1.PrintInfo();
+
+        circle1.SetRadius(5);
+
+        circle1.CalculateArea();
+
+        Circle circle2 = new Circle(3);
+        circle2.PrintInfo();
+
+        circle2.CalculateCircumference();
+    }
+
+    public Circle()
+    {
+        _radius = 0;
+        _diameter = 0;
+    }
+
+    public Circle(double radius)
+    {
+        _radius = radius;
+        _diameter = radius * 2;
+    }
+
+    public void PrintInfo()
+    {
+        if (_radius == 0 && _diameter == 0)
+        {
+            Console.WriteLine("Väärtused pole sisestatud!");
+        }
+        else
+        {
+            Console.WriteLine($"Ringi raadius on {_radius} ja diameeter on {_diameter}");
+        }
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+        _diameter = radius * 2;
+        PrintInfo();
+    }
+
+    public void SetDiameter(double diameter)
+    {
+        _diameter = diameter;
+        _radius = diameter / 2;
+        PrintInfo();
+    }
+
+    public void CalculateArea()
+    {
+        double area = Math.PI * Math.Pow(_radius, 2);
+        Console.WriteLine($"Ringi pindala on {area:F2}");
+    }
+
+    public void CalculateCircumference()
+    {
+        double circumference = 2 * Math.PI * _radius;
+        Console.WriteLine($"Ringi ümbemõõt on {circumference:F2}");
+    }
+}
---
+++ Homework_w6/Homework_w6_2/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Homework_w6_2
+
+public class BankCard
+{
+    static void Main(string[] args)
+    {
+        BankCard card1 = new BankCard();
+        BankCard card2 = new BankCard(100, "Maestro");
+
+        card1.SetCardNumber("12345678");
+        card2.SetCardNumber("123");
+
+        card1.AddMoney(50);
+        card1.PrintAccountBalance();
+
+        card1.SubtractMoney(30);
+        card1.PrintAccountBalance();
+        card1.SubtractMoney(60);
+
+        card1.PrintCardType();
+        card2.PrintCardType();
+    }
+
+    private double Kontojääk;
+    private string Kaarditüüp;
+    private string Kaardinumber;
+
+    public BankCard()
+    {
+        Kontojääk = 0;
+        Kaarditüüp = "Visa";
+    }
+
+    public BankCard(double balance, string type)
+    {
+        Kontojääk = balance;
+        Kaarditüüp = type;
+    }
+
+    public void SetCardNumber(string number)
+    {
+        if (number.Length != 8)
+        {
+            Console.WriteLine("Vale number sisestatud! Kaardil peab olema 8 numbrit!");
+        }
+        else
+        {
+            Kaardinumber = number;
+            Console.WriteLine($"Kaardi number {Kaardinumber} sisestatud edukalt.");
+        }
+    }
+
+    public void PrintCardType()
+    {
+        Console.WriteLine($"Kaarditüüp: {Kaarditüüp}");
+    }
+
+    public void PrintAccountBalance()
+    {
+        Console.WriteLine($"Kontojääk: {Kontojääk}");
+    }
+
+    public double GetAccountBalance()
+    {
+        return Kontojääk;
+    }
+
+    public void AddMoney(double amount)
+    {
+        Kontojääk += amount;
+    }
+
+    public void SubtractMoney(double amount)
+    {
+        if (Kontojääk - amount < 0)
+        {
+            Console.WriteLine("Ei saa teostada makset, pole piisavalt vahendeid");
+        }
+        else
+        {
+            Kontojääk -= amount;
+        }
+    }
+}
